fd23e530e8a90d63775c96987cbaa128a1928683
Tatiana Titova <ritasom666@gmail.com>
2024-01-19 19:35:52 +0400

    reformat code

--- src/test/java/ru/netology/test/CashbackHackTest.java
+++ src/test/java/ru/netology/test/CashbackHackTest.java
@@ -9,47 +9,49 @@ public class CashbackHackTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void shouldReturn100under1000(){
+    public void shouldReturn100under1000() {
         int actual = service.remain(900);
         int expected = 100;
-        Assert.assertEquals(actual, expected );
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldReturn999more1000(){
+    public void shouldReturn999more1000() {
         int actual = service.remain(1001);
         int expected = 999;
         Assert.assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldReturn1before1000(){
+    public void shouldReturn1before1000() {
         int actual = service.remain(999);
         int expected = 1;
         Assert.assertEquals(actual, expected);
     }
+
     @Test
-    public void shouldReturn999before1000(){
+    public void shouldReturn999before1000() {
         int actual = service.remain(1);
         int expected = 999;
         Assert.assertEquals(actual, expected);
     }
+
     @Test
-    public void shouldReturn1more1000(){
+    public void shouldReturn1more1000() {
         int actual = service.remain(1999);
         int expected = 1;
         Assert.assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldReturn0ifMore1000(){
+    public void shouldReturn0ifMore1000() {
         int actual = service.remain(2000);
         int expected = 0;
         Assert.assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldReturn0if1000(){
+    public void shouldReturn0if1000() {
         int actual = service.remain(1000);
         int expected = 0;
         Assert.assertEquals(actual, expected);
