45544c4e62a6495431476831ea80645af0857e45
Tatiana Titova <ritasom666@gmail.com>
2024-01-21 17:41:44 +0400

    add ex.2

--- src/test/java/ru/netology/test/CashbackHackTest.java
+++ src/test/java/ru/netology/test/CashbackHackTest.java
@@ -1,8 +1,10 @@
 package ru.netology.test;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import ru.netology.service.CashbackHackService;
+
 
 public class CashbackHackTest {
 
@@ -55,6 +57,56 @@ public class CashbackHackTest {
         int actual = service.remain(1000);
         int expected = 0;
         Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShouldReturn100under1000() {
+        int actual = service.remain(900);
+        int expected = 100;
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShouldReturn999more1000() {
+        int actual = service.remain(1001);
+        int expected = 999;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShouldReturn1before1000() {
+        int actual = service.remain(999);
+        int expected = 1;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShouldReturn999before1000() {
+        int actual = service.remain(1);
+        int expected = 999;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShouldReturn1more1000() {
+        int actual = service.remain(1999);
+        int expected = 1;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShouldReturn0ifMore1000() {
+        int actual = service.remain(2000);
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShouldReturn0if1000() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
     }
 
 }
